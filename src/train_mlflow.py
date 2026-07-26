4b115de63074add196ee1e15a29ca93f3b4dff40
jstalin08 <jstalin0804@gmail.com>
2026-07-26 22:48:16 +0200

    Clean repository and add MLflow screenhots

--- src/train_mlflow.py
+++ src/train_mlflow.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import mlflow
 import mlflow.sklearn
+import mlflow.xgboost
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
@@ -70,7 +71,8 @@ models = {
         n_estimators=200,
         max_depth=6,
         learning_rate=0.1,
-        random_state=42
+        random_state=42,
+        objective="reg:squarederror"
     )
 }
 
@@ -78,31 +80,44 @@ models = {
 # Train and Log Models
 # =========================
 
-for name, model in models.items():
+for model_name, model in models.items():
 
-    with mlflow.start_run(run_name=name):
+    with mlflow.start_run(run_name=model_name):
 
+        # Train
         model.fit(X_train, y_train)
 
+        # Predict
         preds = model.predict(X_test)
 
+        # Metrics
         mae = mean_absolute_error(y_test, preds)
         rmse = mean_squared_error(y_test, preds) ** 0.5
         r2 = r2_score(y_test, preds)
 
-        mlflow.log_param("model_name", name)
+        # Log Params
+        mlflow.log_param("model_name", model_name)
 
+        # Log Metrics
         mlflow.log_metric("MAE", mae)
         mlflow.log_metric("RMSE", rmse)
         mlflow.log_metric("R2", r2)
 
-        mlflow.sklearn.log_model(
-            sk_model=model,
-            artifact_path="model"
-        )
+        # Log Model
+        if model_name == "XGBoost":
+            mlflow.xgboost.log_model(
+                xgb_model=model,
+                name="model"
+            )
+        else:
+            mlflow.sklearn.log_model(
+                sk_model=model,
+                name="model"
+            )
 
+        # Print Results
         print("\n======================")
-        print(f"Model: {name}")
+        print(f"Model: {model_name}")
         print(f"MAE  : {mae:.6f}")
         print(f"RMSE : {rmse:.6f}")
         print(f"R2   : {r2:.6f}")
